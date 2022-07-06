4c23a31a57e5a2edec881dea6861dfa5af5b4a7e
Enigmataa <Slavyan_bardarski@abv.bg>
2022-07-06 15:13:01 +0300

    deleted commented code

--- test.js
+++ test.js
@@ -1,34 +1,3 @@
-////rectearion of Vladi's stuff
-// fetch("https://jsonplaceholder.typicode.com/users")
-//   .then((response) => response.json())
-//   .then((data) => {
-//     dataFunc(data);
-//   });
-
-// function dataFunc(people) {
-//   people.forEach((person) => {
-//     let container = document.createElement("li");
-
-//     let name = document.createElement("h3"); //makes the element
-//     name.classList.add("name"); //gives it an id
-//     name.innerText = `${person.id} ${person.name}`; //shows it on the page
-
-//     let email = document.createElement("p");
-//     email.classList.add("email");
-//     email.innerText = person.email;
-
-//     let city = document.createElement("p");
-//     city.classList.add("city");
-//     city.innerText = person.address.city;
-
-//     container.appendChild(name);
-//     container.appendChild(email);
-//     container.appendChild(city);
-
-//     document.querySelector("#ppl").appendChild(container);
-//   });
-// }
-
 ////regular cat API
 fetch("https://api.thecatapi.com/v1/breeds")
   .then((response) => response.json())
@@ -60,25 +29,3 @@ function breedsFunction(breed) {
     document.querySelector(".container").appendChild(content);
   });
 }
-///////////////////////////
-// const catPromise = new Promise((resolve, reject) => {
-//   resolve(["Misho", "Lucky", "James", "Bella", "Oscar"]);
-// });
-
-// catPromise
-//   .then((data) => {
-//     return data.map((cat) => `${cat}:3`);
-//   })
-//   .then((data) => {
-//     return data.map((cat) => cat.slice(0, -3));
-//   })
-//   .then((data) => {
-//     return data.sort();
-//   })
-//   .then((data) => data.forEach((cat) => console.log(cat)))
-//   .finally(() => {
-//     console.log("Work is done");
-//   })
-//   .catch((error) => {
-//     console.log(error);
-//   });
